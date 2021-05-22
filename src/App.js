1bc0e82ee45bd1d1b1b3b2059b43cbacabdcbed1
Usama Javed <abbasiusa98@gmail.com>
2021-05-22 10:42:18 +0500

    set background

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ class App extends Component {
       searchField: "",
     };
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
@@ -23,9 +28,10 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1>Monster Rolodex</h1>
         <SearchBox
           placeholder="Search Monsters"
-          handleChange={(e) => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
 
         <CardList monsters={filteredMonsters}></CardList>
